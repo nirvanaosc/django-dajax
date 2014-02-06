6d3d9613185134abc48014c6da19fe5adcb497a0
Steffen Zieger <me@saz.sh>
2014-02-06 11:49:28 +0100

    use json instead of simplejson, fixes jorgebastida/django-dajax#69 and jorgebastida/django-dajax#70

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='django-dajax',
-    version='0.9.2',
+    version='0.9.3',
     author='Jorge Bastida',
     author_email='me@jorgebastida.com',
     description=('Easy to use library to create asynchronous presentation '
@@ -17,7 +17,7 @@ setup(
                       'supports up to four of the most popular JS frameworks: '
                       'jQuery, Prototype, Dojo and mootols.'),
     install_requires=[
-        'django-dajaxice>=0.5'
+        'django-dajaxice>=0.5.6'
     ],
     classifiers=['Development Status :: 4 - Beta',
                 'Environment :: Web Environment',
