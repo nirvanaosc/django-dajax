5a3c0c0e295ef1a1fc61f0c44a1a2035526788e3
Steffen Zieger <me@saz.sh>
2014-02-06 12:19:39 +0100

    update setup.py for pypi release

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 
 setup(
-    name='django-dajax',
+    name='django-dajax-ng',
     version='0.9.3',
-    author='Jorge Bastida',
-    author_email='me@jorgebastida.com',
+    author='Steffen Zieger',
+    author_email='me@saz.sh',
     description=('Easy to use library to create asynchronous presentation '
-                 'logic with django and dajaxice'),
-    url='http://dajaxproject.com',
+                 'logic with django and dajaxice-ng'),
+    url='https://github.com/saz/django-dajax',
     license='BSD',
     packages=['dajax'],
     package_data={'dajax': ['static/dajax/*']},
@@ -15,9 +15,10 @@ setup(
                       'develop asynchronous presentation logic in web '
                       'applications using python and almost no JS code. It '
                       'supports up to four of the most popular JS frameworks: '
-                      'jQuery, Prototype, Dojo and mootols.'),
+                      'jQuery, Prototype, Dojo and mootols.'
+                      'This is the Next Gen dajax maintained by saz.'),
     install_requires=[
-        'django-dajaxice>=0.5.6'
+        'django-dajaxice-ng>=0.5.6.4'
     ],
     classifiers=['Development Status :: 4 - Beta',
                 'Environment :: Web Environment',
